89198438253f4b1968180a85f8c464db1ddb3cee
Obi Chinedu Frank <frankieetchy@gmail.com>
2024-06-04 17:00:52 +0100

    APi

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,13 @@
-export class CreateUserDto {}
+import { IsString, IsEmail, MinLength } from 'class-validator';
+export class CreateUserDto {
+  @IsString()
+  @MinLength(1)
+  first_name: string;
+
+  @IsString()
+  @MinLength(1)
+  last_name: string;
+
+  @IsEmail()
+  email: string;
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,83 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { v4 as uuidv4 } from 'uuid';
+import * as fs from 'fs';
+import { resolve } from 'path';
 
+const filePath = resolve(process.cwd(), './src/db.json');
+
+type User = {
+  id: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+};
+
+type DB = {
+  data: User[];
+};
 @Injectable()
 export class UserService {
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const id = uuidv4();
+    const user = {
+      id,
+      ...createUserDto,
+    };
+    const dbData = this.getFile() as DB;
+    dbData.data = [...dbData.data, user];
+
+    const dataStringify = JSON.stringify(dbData);
+    fs.writeFileSync(filePath, dataStringify);
+    const content = this.getFile();
+    const _user = content.data.find((user) => user.id === id);
+    return _user;
+  }
+
+  getFile(): DB {
+    const data = fs.readFileSync(filePath, { encoding: 'ascii' });
+    return JSON.parse(data);
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.getFile().data;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    const content = this.getFile().data;
+    const user = content.find((user) => user.id === id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  update(id: string, updateUserDto: UpdateUserDto) {
+    console.log({ updateUserDto, id });
+    const dbData = this.getFile();
+    const content = dbData.data;
+    const user = content.find((user) => user.id === id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    let users = content.filter((user) => user.id !== id);
+    const updatedUser = { ...user, ...updateUserDto };
+    users = [...users, updatedUser];
+    dbData.data = users;
+    const dataStringify = JSON.stringify(dbData);
+    fs.writeFileSync(filePath, dataStringify);
+
+    return `user info has been updated`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: string) {
+    const content = this.getFile().data;
+    const users = content.filter((user) => user.id !== id);
+    const dbData = this.getFile() as DB;
+    dbData.data = users;
+    const dataStringify = JSON.stringify(dbData);
+    fs.writeFileSync(filePath, dataStringify);
+    return `user with #${id} has been removed`;
   }
 }
